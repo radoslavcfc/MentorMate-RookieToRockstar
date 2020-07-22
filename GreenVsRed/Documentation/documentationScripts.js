0e17de6f5cfbb53717699ef465988f9044944f9f
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-07-22 11:13:04 +0300

    Scripts sorted

--- GreenVsRed/Documentation/documentationScripts.js
+++ GreenVsRed/Documentation/documentationScripts.js
@@ -1,23 +1,33 @@
+//Initialize home content
+var homeHtml = document.getElementById("home");
+var mainDoc = document.getElementById("mainArea");
+
+function Initialize(){
+    mainDoc.innerHTML = homeHtml.innerHTML;
+}
+
+Initialize();
+
 //DropDown function:
-    var dropdown = document.getElementsByClassName("dropdown-btn");
-    var i;
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
 
-    for (i = 0; i < dropdown.length; i++) {
-         dropdown[i].addEventListener("click", function() {
-             this.classList.toggle("active");
-             var dropdownContent = this.nextElementSibling;
-            if (dropdownContent.style.display === "block") {
-                dropdownContent.style.display = "none";
-            } 
-            else {
-             dropdownContent.style.display = "block";
-            }
-         });
-    }
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+       this.classList.toggle("active");
+       var dropdownContent = this.nextElementSibling;
+       if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+       } 
+       else {
+           dropdownContent.style.display = "block";
+       }
+    });
+}
 
-    //Main content toggle function
-    var mainDoc = document.getElementById("mainArea");
-    function Toggle(currentId){
-        var currentElement = document.getElementById(currentId);
-        mainDoc.innerHTML = currentElement.innerHTML;
-    }
+//Main content toggle function
+
+function Toggle(currentId){
+    var currentElement = document.getElementById(currentId);
+    mainDoc.innerHTML = currentElement.innerHTML;
+}
